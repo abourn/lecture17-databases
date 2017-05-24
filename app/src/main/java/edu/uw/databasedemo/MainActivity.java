54417fe73b15dc5971ba8fc36e7d2ce6342fe726
Joel Ross <joelross@uw.edu>
2017-05-24 14:26:56 -0700

    Reset for start of lecture

--- app/src/main/java/edu/uw/databasedemo/MainActivity.java
+++ app/src/main/java/edu/uw/databasedemo/MainActivity.java
@@ -3,10 +3,9 @@ package edu.uw.databasedemo;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -38,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 this,
                 R.layout.list_item_layout, //item to inflate
                 null, //cursor to show
-                new String[] {WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT}, //fields to display
+                new String[] {UserDictionary.Words.WORD, UserDictionary.Words.FREQUENCY}, //fields to display
                 new int[] {R.id.txtItemWord, R.id.txtItemFreq},                           //where to display them
                 0);
         listView.setAdapter(adapter);
@@ -59,8 +58,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Cursor item = (Cursor)parent.getItemAtPosition(position); //item we clicked on
-                String word = item.getString(item.getColumnIndexOrThrow(WordDatabase.WordEntry.COL_WORD));
-                int freq = item.getInt(item.getColumnIndexOrThrow(WordDatabase.WordEntry.COL_COUNT));
+                String word = item.getString(item.getColumnIndexOrThrow(UserDictionary.Words.WORD));
+                int freq = item.getInt(item.getColumnIndexOrThrow(UserDictionary.Words.FREQUENCY));
                 Log.v(TAG, "Clicked on '"+word+"' ("+freq+")");
 
                 setFrequency(id, freq+1);
@@ -74,11 +73,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         TextView inputText = (TextView)findViewById(R.id.txtAddWord);
 
         ContentValues newValues = new ContentValues();
-        newValues.put(WordDatabase.WordEntry.COL_WORD, inputText.getText().toString());
-        newValues.put(WordDatabase.WordEntry.COL_COUNT, 0);
+        newValues.put(UserDictionary.Words.WORD, inputText.getText().toString());
+        newValues.put(UserDictionary.Words.FREQUENCY, 100);
+        newValues.put(UserDictionary.Words.APP_ID, "edu.uw.databasedemo");
+        newValues.put(UserDictionary.Words.LOCALE, "en_US");
 
         Uri newUri = this.getContentResolver().insert(
-                WordProvider.CONTENT_URI,   // our provider!
+                UserDictionary.Words.CONTENT_URI,   // the user dictionary content URI!
                 newValues                   // the values to insert
         );
         Log.v(TAG, "New word at: "+newUri);
@@ -87,10 +88,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     //sets (updates) the frequency of the word with the given id
     public void setFrequency(long id, int newFrequency){
         ContentValues newValues = new ContentValues();
-        newValues.put(WordDatabase.WordEntry.COL_COUNT, newFrequency);
+        newValues.put(UserDictionary.Words.FREQUENCY, newFrequency);
 
         this.getContentResolver().update(
-                ContentUris.withAppendedId(WordProvider.CONTENT_URI, id),
+                ContentUris.withAppendedId(UserDictionary.Words.CONTENT_URI, id),
                 newValues,
                 null, null); //no selection
     }
@@ -98,12 +99,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         //fields to fetch from the provider
-        String[] projection = {WordDatabase.WordEntry._ID, WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT};
+        String[] projection = {UserDictionary.Words._ID, UserDictionary.Words.WORD, UserDictionary.Words.FREQUENCY};
 
         //create the CursorLoader to fetch data from the content provider
         CursorLoader loader = new CursorLoader(
                 this,
-                WordProvider.CONTENT_URI,
+                UserDictionary.Words.CONTENT_URI,
                 projection,
                 null, //no filter
                 null,
@@ -136,22 +137,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.menu_test:
-                WordDatabase.DatabaseHelper helper = new WordDatabase.DatabaseHelper(this);
-                SQLiteDatabase db = helper.getReadableDatabase();
 
-                SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
-                builder.setTables(WordDatabase.WordEntry.TABLE_NAME);
-
-                Cursor results = builder.query(
-                        db,
-                        new String[] {WordDatabase.WordEntry.COL_WORD, WordDatabase.WordEntry.COL_COUNT},
-                        null, null, null, null, null);
-
-                while(results.moveToNext()){
-                    String word = results.getString(results.getColumnIndexOrThrow(WordDatabase.WordEntry.COL_WORD));
-                    int freq = results.getInt(results.getColumnIndexOrThrow(WordDatabase.WordEntry.COL_COUNT));
-                    Log.v(TAG, "'"+word+"' ("+freq+")");
-                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
